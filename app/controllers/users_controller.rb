8e82c18a08c9e25ad337568942e465402bdcc2a2
lokesh-karthikeyan <172601273+lokesh-karthikeyan@users.noreply.github.com>
2025-05-01 09:47:30 +0530

    Controller: Add `#create` for user registration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,28 @@
 class UsersController < ApplicationController
   allow_unauthenticated_access only: [ :new ]
+  skip_before_action :require_authentication, only: [ :create ]
 
   def new
+  end
+
+  def create
+    user_class = params[:role].camelize.constantize
+    @user = user_class.new(
+      full_name: params[:full_name],
+      email_address: params[:email_address],
+      password: params[:password]
+    )
+
+    if @user.save
+      start_new_session_for(@user)
+
+      if @user.is_a?(Doctor)
+        redirect_to(doctor_path(@user), notice: "Signed in successfully!")
+      else
+        redirect_to(receptionist_path(@user), notice: "Signed in successfully")
+      end
+    else
+      redirect_to(root_path, status: :unprocessable_entity, alert: "Unable to process this request")
+    end
   end
 end
