878d38fbd354dfd9abdd3f216972db9ee8cdd23c
lokesh-karthikeyan <172601273+lokesh-karthikeyan@users.noreply.github.com>
2025-05-01 16:57:43 +0530

    Fix: Correct the `null` values & column name
     + Added `optional: true` to accept empty values for doctor's reference.
     + Replace the incorrect `date` -> `appointment_date` in validations.

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,11 +1,11 @@
 class Patient < ApplicationRecord
   belongs_to :receptionist, class_name: "User", foreign_key: "receptionist_id"
-  belongs_to :doctor, class_name: "User", foreign_key: "doctor_id"
+  belongs_to :doctor, class_name: "User", foreign_key: "doctor_id", optional: true
 
   validates :name, presence: true
   validates :age, presence: true, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 100 }
   validates(
-    :date,
+    :appointment_date,
     presence: true,
     inclusion: { in: Date.today..Date::Infinity.new, message: "must be today or in the future." }
   )
