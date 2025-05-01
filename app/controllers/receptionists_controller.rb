07aba0e6528ea61687784d005dc2dda05bc117f2
lokesh-karthikeyan <172601273+lokesh-karthikeyan@users.noreply.github.com>
2025-05-01 16:56:11 +0530

    Controller: Add `#show` to display receptionist dashboard

--- app/controllers/receptionists_controller.rb
+++ app/controllers/receptionists_controller.rb
@@ -1,2 +1,6 @@
 class ReceptionistsController < ApplicationController
+  def show
+    @name = Current.user.full_name.split(" ").first.capitalize
+    @patients = Patient.where(receptionist_id: Current.user.id).order(created_at: :desc)
+  end
 end
