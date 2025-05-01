1803bd7d3525686fe210e09ab0b7925de2e95b10
lokesh-karthikeyan <172601273+lokesh-karthikeyan@users.noreply.github.com>
2025-05-01 09:43:43 +0530

    Sec(Auth): Add post authentication routes based on role

--- app/controllers/concerns/authentication.rb
+++ app/controllers/concerns/authentication.rb
@@ -8,45 +8,53 @@ module Authentication
 
   class_methods do
     def allow_unauthenticated_access(**options)
-      skip_before_action :require_authentication, **options
+      skip_before_action(:require_authentication, **options)
     end
   end
 
   private
-    def authenticated?
-      resume_session
-    end
+  def authenticated?
+    resume_session
+  end
 
-    def require_authentication
-      resume_session || request_authentication
-    end
+  def require_authentication
+    resume_session || request_authentication
+  end
 
-    def resume_session
-      Current.session ||= find_session_by_cookie
-    end
+  def resume_session
+    Current.session ||= find_session_by_cookie
+  end
 
-    def find_session_by_cookie
-      Session.find_by(id: cookies.signed[:session_id]) if cookies.signed[:session_id]
-    end
+  def find_session_by_cookie
+    Session.find_by(id: cookies.signed[:session_id]) if cookies.signed[:session_id]
+  end
 
-    def request_authentication
-      session[:return_to_after_authenticating] = request.url
-      redirect_to new_session_path
-    end
+  def request_authentication
+    session[:return_to_after_authenticating] = request.url
+    redirect_to(new_session_path)
+  end
 
-    def after_authentication_url
-      session.delete(:return_to_after_authenticating) || root_url
-    end
-
-    def start_new_session_for(user)
-      user.sessions.create!(user_agent: request.user_agent, ip_address: request.remote_ip).tap do |session|
-        Current.session = session
-        cookies.signed.permanent[:session_id] = { value: session.id, httponly: true, same_site: :lax }
+  def after_authentication_url
+    session.delete(:return_to_after_authenticating) ||
+      case Current.session.user
+      when Doctor
+        doctor_path(Current.user)
+      when Receptionist
+        receptionist_path(Current.user)
+      else
+        root_url
       end
-    end
+  end
 
-    def terminate_session
-      Current.session.destroy
-      cookies.delete(:session_id)
+  def start_new_session_for(user)
+    user.sessions.create!(user_agent: request.user_agent, ip_address: request.remote_ip).tap do |session|
+      Current.session = session
+      cookies.signed.permanent[:session_id] = { value: session.id, httponly: true, same_site: :lax }
     end
+  end
+
+  def terminate_session
+    Current.session.destroy
+    cookies.delete(:session_id)
+  end
 end
