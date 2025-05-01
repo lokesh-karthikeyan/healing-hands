73d3d878b41ea63f2a335c7d9b4276c70b6f1656
lokesh-karthikeyan <172601273+lokesh-karthikeyan@users.noreply.github.com>
2025-05-01 10:54:28 +0530

    Model: Add `data validations` & `associations`

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,3 +1,12 @@
 class Patient < ApplicationRecord
-  belongs_to :receptionist
+  belongs_to :receptionist, class_name: "User", foreign_key: "receptionist_id"
+  belongs_to :doctor, class_name: "User", foreign_key: "doctor_id"
+
+  validates :name, presence: true
+  validates :age, presence: true, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 100 }
+  validates(
+    :date,
+    presence: true,
+    inclusion: { in: Date.today..Date::Infinity.new, message: "must be today or in the future." }
+  )
 end
