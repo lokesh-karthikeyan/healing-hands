05e7d08793ad2925c53949f01245760155270c62
lokesh-karthikeyan <172601273+lokesh-karthikeyan@users.noreply.github.com>
2025-05-01 15:21:47 +0530

    Controller: Add `#create` action to create patients

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,2 +1,23 @@
 class PatientsController < ApplicationController
+  def create
+    @patient = Patient.new(
+      name: params[:name],
+      age: params[:age],
+      appointment_date: params[:appointment_date],
+      receptionist_id: Current.user.id
+    )
+
+    if @patient.save
+      respond_to do |format|
+        format.turbo_stream
+        format.html { redirect_to(receptionist_path(Current.user), notice: "Appointment is booked!") }
+      end
+    else
+      redirect_to(
+        receptionist_path(Current.user),
+        status: :unprocessable_entity,
+        alert: "Unable to process this request"
+      )
+    end
+  end
 end
