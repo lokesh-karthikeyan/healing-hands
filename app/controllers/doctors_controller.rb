3fc4de2ef456d272318fcab30a158b87c86cf86f
lokesh-karthikeyan <172601273+lokesh-karthikeyan@users.noreply.github.com>
2025-05-01 20:08:35 +0530

    Controller: Add `#show` to view doctor's dashboard

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -1,2 +1,14 @@
 class DoctorsController < ApplicationController
+  def show
+    @name = Current.user.full_name.split(" ").first.capitalize
+    @patients = Patient.all
+    @patient_counts = Patient.group_by_day(:created_at).count
+    @appointment_counts = Patient.group_by_day(:appointment_date).count
+
+    if params[:query].present?
+      @patients = Patient.where("name LIKE ?", "%#{params[:query]}%")
+    else
+      @patients = Patient.all
+    end
+  end
 end
