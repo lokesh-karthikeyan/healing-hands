03c3e106ef78080ca1abe1979b906acd04e2470c
lokesh-karthikeyan <172601273+lokesh-karthikeyan@users.noreply.github.com>
2025-05-01 10:51:56 +0530

    DB: Define custom `references` columns

--- db/migrate/20250501045954_create_patients.rb
+++ db/migrate/20250501045954_create_patients.rb
@@ -1,10 +1,12 @@
 class CreatePatients < ActiveRecord::Migration[8.0]
   def change
-    create_table :patients do |t|
-      t.string :name
-      t.integer :age
-      t.date :appointment_date
-      t.references :receptionist, null: false, foreign_key: true
+    create_table(:patients) do |t|
+      t.string(:name)
+      t.integer(:age)
+      t.date(:appointment_date)
+      t.text(:remarks)
+      t.references(:receptionist, foreign_key: { to_table: :users }, null: false)
+      t.references(:doctor, foreign_key: { to_table: :users }, null: true)
 
       t.timestamps
     end
